627fd857e4c1e8cc57f77a2b4ce22fd3d0784835
Aayush Malde <1000075545@hexaware.com>
2022-07-13 14:52:44 -0400

    case 2 without DB.java implementation

--- src/com/main/CustomerMenuService.java
+++ src/com/main/CustomerMenuService.java
@@ -1,14 +1,22 @@
 package com.main;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+import com.main.db.DB;
+import com.main.model.Product;
 
 public class CustomerMenuService {
 
 	private Scanner sc = new Scanner(System.in);
+	DB db = new DB();
 
 	public int displayCustomerMenuAndReadInput() {
 		System.out.println("******Customer Main Menu******");
-		System.out.println("1. View Items");
+		System.out.println("1. View Inventory");
 		System.out.println("2. Buy Item");
 		System.out.println("3. Cancel Purchase");
 		System.out.println("4. View/Edit Balance");
@@ -25,9 +33,76 @@ public class CustomerMenuService {
 		// TODO Auto-generated method stub
 		switch(customerInput) {
 		case 1:
+			System.out.println("******View Inventory******");
+			System.out.println("1. View all");
+			System.out.println("2. Filter results");
+			System.out.println("Enter one of the above options: ");
 			
+			List<Product> list;
+			int input = sc.nextInt();
+			if(input == 2) {
+				System.out.println("Enter a column to filter by: ");
+				String columnFilter = sc.next();
+				
+				System.out.println("Enter a string to filter by: ");
+				String stringFilter = sc.next();
+				
+				list = db.fetchInventory(columnFilter, stringFilter);
+			}else {
+				list = db.fetchInventory();
+			}
+			
+			System.out.println("------------------------------------------------------------------------------------------------");
+			for(Product p : list) {
+				System.out.println(p.toString());
+				System.out.println("------------------------------------------------------------------------------------------------");
+			}
+			System.out.println();
 			break;
+		
+		
 		case 2:
+			sc.nextLine();
+			System.out.println("Enter product name: ");
+			String inp = sc.next();
+			//Return list of that productName
+			List<Product> list2;
+			list2 = db.fetchInventory(inp,"productName");
+			System.out.println("------------------------------------------------------------------------------------------------");
+			for(Product p : list2) {
+				System.out.println(p.toString());
+				System.out.println("------------------------------------------------------------------------------------------------");
+			}
+			//check if quantity is available
+			int quantity = list2.get(0).getQuantity();
+			if(quantity<=0) {
+				System.out.println("Error: Out of Stock");
+				break;
+			}
+			
+			System.out.println("Enter quantity: ");
+			input = sc.nextInt();
+			if(input>quantity||input==0) {
+				System.out.println("Error: Quantity not available");
+				break;
+			}
+			
+			//check if user can purchase it
+			double balance = 100; //temp value
+			double price = quantity*list2.get(0).getPrice();
+			if(price>balance) {
+				System.out.println("Error: Not enough balance");
+				System.out.println("The price is "+price);
+				System.out.println("Your current balance is "+balance);
+				break;
+			}
+			
+			System.out.println("You have completed the purchase");
+			//Need a DB.java method
+
+			
+			
+			
 			break;
 			
 		case 3:
@@ -36,7 +111,11 @@ public class CustomerMenuService {
 		case 4:
 			break;
 			
+		default:
+			System.out.println("Invalid input");
+			break;
+		}
 		
 		}
 	}
-}
+
